d123686c181e90ad9334401e8de40957e3238eab
Flauber Sauan Cândido Alves <20231101110029@ifrn.local>
2025-12-17 17:36:33 -0300

    Finalizando o final

--- app.py
+++ app.py
@@ -21,28 +21,6 @@ app.register_blueprint(horario_bp)
 
 session = Session()
 
-horas = [
-    time(8, 0),
-    time(9, 0),
-    time(10, 0),
-    time(11, 0),
-    time(13, 0),
-    time(14, 0),
-    time(15, 0),
-    time(16, 0),
-    time(17, 0),
-]
-
-hoje = date.today()
-
-for h in horas:
-    existe = session.query(Horario).filter_by(data=hoje, hora=h).first()
-    if not existe:
-        session.add(Horario(data=hoje, hora=h))
-
-session.commit()
-session.close()
-
 
 @login_manager.user_loader
 def load_user(user_id):
